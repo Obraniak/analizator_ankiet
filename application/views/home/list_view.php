4d5e58271f7fea56fa863cc63e22ef5744709d5a
Marcin Burgknap <marcin.burgknap@gmail.com>
2013-06-07 20:04:14 +0200

    fix

--- application/views/home/list_view.php
+++ application/views/home/list_view.php
@@ -1,6 +1,11 @@
 <div class="app_content">
-	<?php echo 'Użytkownik zalogowany jako ' . $user; ?>
+	<?php  echo lang('user_loged') . ' ' . $user; ?>
 
+<div>
+	<?php echo '<form action="' . site_url('/login') . '">'; ?>
+	<input type="submit" value="<?php echo lang('logout'); ?>">
+	<?php 	echo '</form>'; ?>
+</div>
 	<div>
 		<table border="1" class="kursy">
 			<tr>
@@ -18,27 +23,27 @@
 				<input type="text" name="lstaus">
 				</td>
 				<td>
-				<input type="submit" value="Szukaj">
+				<input type="submit" value="<?php echo lang('search'); ?>">
 				</td>
 				<?php 	echo '</form>'; ?>
 			</tr>
 			<tr>
-				<td>Kurs</td>
-				<td>Nazwa ankiety</td>
-				<td>Data</td>
-				<td>Status</td>
-				<td>Akcja</td>
+				<td><?php echo lang('course'); ?></td>
+				<td><?php echo lang('form_name'); ?></td>
+				<td><?php echo lang('date'); ?></td>
+				<td><?php echo lang('status'); ?></td>
+				<td><?php echo lang('action'); ?></td>
 			</tr>
 			<?php
-				foreach ($form_list as $form) {
-					echo '<tr>';
-					echo '<td>' . $form -> course . '</td>';
-					echo '<td>' . $form -> name . '</td>';
-					echo '<td>' . $form -> date . '</td>';
-					echo '<td>' . $form -> status . '</td>';
-					echo '<td><a class="btnakcja" href="' . site_url('/form/start/id/' . $form -> id) . '">Przejdz</a></td>';
-					echo "</tr>";
-				}
+			foreach ($form_list as $form) {
+				echo '<tr>';
+				echo '<td>' . $form -> course . '</td>';
+				echo '<td>' . $form -> name . '</td>';
+				echo '<td>' . $form -> date . '</td>';
+				echo '<td>' . $form -> status . '</td>';
+				echo '<td><a class="btnakcja" href="' . site_url('/form/start/id/' . $form -> id) . '">' . lang('goto') . '</a></td>';
+				echo "</tr>";
+			}
 			?>
 		</table>
 	</div>
